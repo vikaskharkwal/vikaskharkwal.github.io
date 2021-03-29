0ed871d35d599f8c2988e46279e5e4f1a832924a
Vikas Kharkwal <weyvik@github.com>
2021-03-29 10:26:26 +0530

    fixed minor bug related to resize event listener

--- app.js
+++ app.js
@@ -80,10 +80,10 @@ function menuEvent() {
     }
 }
 
-console.log(window.innerHeight);
+root.style.setProperty('--vh', window.innerHeight / 100 + 'px');
 
 window.addEventListener('resize', () => {
-    document.querySelector(':root').style
-        .setProperty('--vh', window.innerHeight / 100 + 'px');
+    root.style.setProperty('--vh', window.innerHeight / 100 + 'px');
+    console.log(window.innerHeight);
 })
 
